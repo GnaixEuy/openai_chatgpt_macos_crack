8a3ad8698cfab7210c1a40b07a51e14a9bba5720
GnaixEuy <SGGHCJ@live.com>
2024-05-15 13:04:44 +0800

    :beer: commit script

--- crack_chatgpt_macos.js
+++ crack_chatgpt_macos.js
@@ -1,28 +1,32 @@
+// 修改响应状态码为200
+let status = 200;
+
 // 获取响应的原始body
 let body = $response.body;
 
-// 将body解析为JSON对象
-let obj = JSON.parse(body);
+// 如果响应内容是JSON，且需要修改其内容，可以在此进行处理
+try {
+    let obj = JSON.parse(body);
 
-// 定义一个递归函数来遍历对象并替换false值为true
-function replaceFalseWithTrue(obj) {
-    for (let key in obj) {
-        if (obj[key] === false) {
-            obj[key] = true;
-        } else if (typeof obj[key] === 'object' && obj[key] !== null) {
-            replaceFalseWithTrue(obj[key]);
+    // 对JSON对象进行处理，如果有需要，可以替换false为true
+    function replaceFalseWithTrue(obj) {
+        for (let key in obj) {
+            if (obj[key] === false) {
+                obj[key] = true;
+            } else if (typeof obj[key] === 'object' && obj[key] !== null) {
+                replaceFalseWithTrue(obj[key]);
+            }
         }
     }
+
+    // 调用递归函数
+    replaceFalseWithTrue(obj);
+
+    // 将修改后的对象重新转换为JSON字符串
+    body = JSON.stringify(obj);
+} catch (e) {
+    // 如果不是JSON响应，则不做处理
 }
 
-// 调用递归函数
-replaceFalseWithTrue(obj);
-
-// 将修改后的对象重新转换为JSON字符串
-body = JSON.stringify(obj);
-
-// 返回修改后的body
-$done({ body });
-
-//
-// ^https://ab.chatgpt.com/v1/initialize$ url script-response-body crack_chatgpt_macos.js
+// 返回修改后的状态码和body
+$done({ status, body });
